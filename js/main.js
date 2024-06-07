91db4cb60caa77889d6e8a616a7bf116aa6a1691
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-07 15:24:10 +0200

    minimo output in pagina html

--- js/main.js
+++ js/main.js
@@ -1,38 +1,68 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-// let mailUser = prompt("qual è la tua mail?")
-// const mailDB = ["mario@gmail.com","filippo@gmail.com","alessia@gmail.com","jhonny@gmail.com"]
-// const mailContainer = document.getElementById("mail_container")
+let mailUser = prompt("qual è la tua mail?")
+const mailDB = ["mario@gmail.com","filippo@gmail.com","alessia@gmail.com","jhonny@gmail.com"]
+const mailContainer = document.getElementById("mail_container")
 
-// for (let i = 0; i < mailDB.length; i++){
-//     let listMail = mailDB[i]
+for (let i = 0; i < mailDB.length; i++){
+    let listMail = mailDB[i]
 
-//     if (mailUser === listMail){
-//         console.log("mail presente nel database")
-//         mailContainer.innerHTML = "<li>presente</li>"
-//     } else if (mailUser !== listMail){
-//         console.log("Tu Non Puoi Passareeee!!")
-//         mailContainer.innerHTML = "<li>Non Presente</li>"
-//     } 
-// }
+    if (mailUser === listMail){
+        console.log("mail presente nel database")
+        mailContainer.innerHTML = "<li>presente</li>"
+    } else if (mailUser !== listMail){
+        console.log("Tu Non Puoi Passareeee!!")
+        mailContainer.innerHTML = "<li>Non Presente</li>"
+    } 
+}
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 const dadiContainer = document.getElementById("dadi_container");
 
+dadiContainer.addEventListener("click",
+    () => {
+        // Gnerazione numeri randomici giocatore e pc
+        let numPl = Math.floor(Math.random() * 6)
+        let numPc = Math.floor(Math.random() * 6)
+
+        // Creazion elemento p in pagina
+        let newP = document.createElement("p")
+        // Elemento appena creato attacoto al div dadiContainer
+        dadiContainer.append(newP)
+        // Stampa in pagina
+        newP.innerHTML = `<h3>IL NUMERO DEL GIOCATORE E' ${numPl}</h3><h3>IL NUMERO DEL PC E' ${numPc}`
+        console.log("Numero del Giocatore", numPl)
+        console.log("numero del PC", numPc)
+        // comparazione di victa
+        if (numPl > numPc) {
+            let myP = document.getElementById("risultato")
+            myP.append("Il giocatore ha vinto!!")
+            console.log("Giocatore vincente")
+        } else if (numPc > numPl) {
+            let myP = document.getElementById("risultato")
+            myP.append("Il PC ha vinto!!")
+            console.log("Pc vincente")
+        } else {
+            let myP = document.getElementById("risultato")
+            myP.append("Pareggio!!")
+            console.log("Pareggio")
+        }
+    }
+)
 // Gnerazione numeri randomici giocatore e pc
-let numPl = Math.floor(Math.random() * 6);
-let numPc = Math.floor(Math.random() * 6);
-console.log("Numero del Giocatore", numPl)
-console.log("numero del PC", numPc)
-// comparazione di victa
-if (numPl > numPc) {
-    console.log("Giocatore vincente")
-} else if (numPc > numPl) {
-    console.log("Pc vincente")
-} else {
-    console.log("Pareggio")
-}
+// let numPl = Math.floor(Math.random() * 6);
+// let numPc = Math.floor(Math.random() * 6);
+// console.log("Numero del Giocatore", numPl)
+// console.log("numero del PC", numPc)
+// // comparazione di victa
+// if (numPl > numPc) {
+//     console.log("Giocatore vincente")
+// } else if (numPc > numPl) {
+//     console.log("Pc vincente")
+// } else {
+//     console.log("Pareggio")
+// }
 
 
 
